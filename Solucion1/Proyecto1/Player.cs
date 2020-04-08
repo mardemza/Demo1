fcde338165dcdc8c01ec073b0abef04070aed608
Moises Rivas <mardemza@hotmail.com>
2020-04-08 19:51:30 -0300

    Subiendo proyectos

---
+++ Solucion1/Proyecto1/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto1
+{
+    public class Player
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private int Number { get; set; }
+
+        /// <summary>
+        /// Parametros de entrada
+        /// </summary>
+        /// <param name="id">Registro</param>
+        /// <param name="name">Nombre</param>
+        /// <param name="surname">Apellido</param>
+        /// <param name="number">Numero de camiseta</param>
+        public Player(int id,string name, string surname, int number)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Number = number;
+        }
+
+
+        public string FullName()
+        {
+            return $"{Name} {Surname} | {Number}";
+        }
+    }
+}
---
+++ Solucion1/Proyecto1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Proyecto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // -- Asigno jugador
+            var player1 = new Player(1,"Carlos","Tevez",10);
+
+            Console.WriteLine("Jugador: {0}",player1.FullName());
+            var text = string.Format("{0} {1}, hoy es {2}", "Bienvenidos", "Juan", DateTime.Now.ToString("D"));
+            
+            Console.WriteLine(text);
+
+            Console.WriteLine(" =============================== ");
+            Console.WriteLine(" =                             = ");
+            Console.WriteLine(" =     Por favor ingrese       = ");
+            Console.WriteLine(" =        su nombre            = ");
+            Console.WriteLine(" =                             = ");
+            Console.WriteLine(" =============================== ");
+
+            var nombre = Console.ReadLine();
+             //cambios nuevos
+            Console.WriteLine(" =============================== ");
+            Console.WriteLine(" =                             = ");
+            Console.WriteLine(" =        Su nombre es         = ");
+            Console.WriteLine($" =         {nombre}           = ");
+            Console.WriteLine(" =                             = ");
+            Console.WriteLine(" =============================== ");
+
+        }
+    }
+}
